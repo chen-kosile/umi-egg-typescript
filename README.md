420d5efea49017e1cd5748edf866a7024b43d6af
chenyuhan1 <1944614333@qq.com>
2019-05-17 21:07:18 +0800

    fullStack

---
+++ README.md
@@ -0,0 +1,36 @@
+# 用于应用学习的知识
+
+- typescript
+- umi
+- egg
+- echarts
+- WebGL
+- node
+- react
+- dva
+
+- 项目目录结构
+  - egg-mysql node后端
+    - app  主要后端代码
+      - controller
+      - extend
+      - middleware
+      - model
+      - public
+      - service
+    - config  egg 配置文件
+    - database 数据库配置文件 有sequelize来映射数据库
+  - react-ts-umi react前端
+    - config  react 项目配置 umi框架配置
+    - dist 项目打包后的项目
+    - mock 数据模拟文件 遵循umi配置
+    - public
+    - src 项目主要代码
+      - assets 一些静态文件， 图片
+      - components 公用组件
+      - config 项目中有些公用数据配置文件
+      - layouts 各主页面页面布局
+      - models dva model数据流控制
+      - pages umi约定式页面
+      - services 服务文件
+      - utils 工具文件 request等
---
+++ react-ts-umi/src/models/assist.ts
@@ -0,0 +1,50 @@
+// import {} from 'utils/request';
+import { Effect } from '@/models/connect';
+import { Reducer } from 'redux';
+
+// 工具model
+export interface IAssistModelState {
+  test: [],
+  name: string
+}
+
+export interface IAssistModel {
+  name: 'assist',
+  state: IAssistModelState,
+  effects: {
+    test: Effect
+  },
+  reducers: {
+    save: Reducer<any>,
+    test: Reducer<any>
+  }
+}
+
+const AssistModel: IAssistModel = {
+  name: 'assist',
+  state: {
+    test: [],
+    name: 'test'
+  },
+  effects: {
+    *test({ payload }, { call, put, select }) {
+      let test = yield 'test';
+    }
+  },
+  reducers: {
+    save(state, action) {
+      return {
+        ...state,
+        ...action.payload
+      }
+    },
+    test(state, action) {
+      return {
+        ...state,
+        ...action.payload
+      }
+    }
+  }
+}
+
+export default AssistModel;
